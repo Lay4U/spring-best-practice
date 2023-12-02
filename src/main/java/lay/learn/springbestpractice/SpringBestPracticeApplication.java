c5597c8009737fbe9c2434dd308fbb3625a50e8a
Lay <kmr345@naver.com>
2023-12-02 14:50:45 +0900

    Initial commit

---
+++ src/main/java/lay/learn/springbestpractice/SpringBestPracticeApplication.java
@@ -0,0 +1,13 @@
+package lay.learn.springbestpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBestPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBestPracticeApplication.class, args);
+    }
+
+}
